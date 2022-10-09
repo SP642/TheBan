e3d1926b4c40ba21b96a38a21621c13fc0a9a686
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2022-10-09 18:21:10 +0000

    use expressjs

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+nunjucks.configure('templates', { autoescape: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/img', express.static('img'));
 
 app.get('/', async function (req, res) {
-  return res.sendFile("temp.html");
+  return res.send(nunjucks.render("index.html", {}));
 });
 
 app.listen(8081, () => {
